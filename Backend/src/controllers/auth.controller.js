6bd23c44e4ec92a7d263888147f56722b19c0e11
Tejas <tejassangale90@gmail.com>
2026-06-26 13:12:40 +0530

    cookie issue solved

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -74,7 +74,7 @@ async function loginUserController(req, res){
     {expiresIn: "1d"}
     )
 
-    res.cookie("token", token, { httpOnly: true, sameSite: 'lax' })
+    res.cookie("token", token, { httpOnly: true, sameSite: 'none', secure:true })
     res.status(200).json({
         message: "User logged in Successfully",
         user:{
